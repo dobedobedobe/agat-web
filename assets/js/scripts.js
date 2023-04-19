f4c828f200a336c387ac3a6a4717a85a6cca8d8e
ambikajo <ambikajoshi@gmail.com>
2023-04-19 20:51:33 +0530

    init

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,9 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
+
+var c = document.getElementById("salad");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
